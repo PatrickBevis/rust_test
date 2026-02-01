345c5f40118b906347170b39b06a7a894827ba5c
Patrick <bevis.patrick@gmail.com>
2026-02-01 18:27:56 +0100

    less or more

---
+++ rust_test/target/debug/deps/Rust_test-3b7ad7f8ae41b72e.d
@@ -0,0 +1,5 @@
+/home/patrick/Projets/rust_test/rust_test/target/debug/deps/Rust_test-3b7ad7f8ae41b72e.d: src/main.rs
+
+/home/patrick/Projets/rust_test/rust_test/target/debug/deps/libRust_test-3b7ad7f8ae41b72e.rmeta: src/main.rs
+
+src/main.rs:
---
+++ rust_test/target/debug/deps/Rust_test-bbf0fa3575aa22e8.d
@@ -0,0 +1,5 @@
+/home/patrick/Projets/rust_test/rust_test/target/debug/deps/Rust_test-bbf0fa3575aa22e8.d: src/main.rs
+
+/home/patrick/Projets/rust_test/rust_test/target/debug/deps/libRust_test-bbf0fa3575aa22e8.rmeta: src/main.rs
+
+src/main.rs:
